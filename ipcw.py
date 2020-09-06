4d14d8f6a226ba0d1586cb28c732c4588aa35a7d
Sebastian Stetter <tuxintrouble>
2020-09-07 00:11:13 +0200

    1st rought prototype implementation of an ip cw protocol and server client architecture

---
+++ ipcw.py
@@ -0,0 +1,186 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+#Author: Sebastian Stetter  (DJ5SE)
+#License GPL v3
+
+#This file implements funktions for cw over the UDP protocol
+
+import socket, time
+from math import ceil
+
+DEBUG=0
+
+protocol_version = 1
+serial = 1
+
+morse = {"0" : "-----", "1" : ".----", "2" : "..---", "3" : "...--", "4" : "....-", "5" : ".....",
+         "6" : "-....", "7" : "--...", "8" : "---..", "9" : "----.",
+         "a" : ".-", "b" : "-...", "c" : "-.-.", "d" : "-..", "e" : ".", "f" : "..-.", "g" : "--.",
+         "h" : "....", "i" : "..", "j" : ".---", "k" : "-.-", "l" : ".-..", "m" : "--", "n" : "-.",
+         "o" : "---", "p" : ".--.", "q" : "--.-", "r" : ".-.", "s" : "...", "t" : "-", "u" : "..-",
+         "v" : "...-", "w" : ".--", "x" : "-..-", "y" : "-.--", "z" : "--..", "=" : "-...-",
+         "/" : "-..-.", "+" : ".-.-.", "-" : "-....-", "." : ".-.-.-", "," : "--..--", "?" : "..--..",
+         ":" : "---...", "!" : "-.-.--", "'" : ".----."
+    }
+
+def debug(s):
+    if DEBUG:
+        print(s)
+
+
+def zfill(str,digits):
+    '''we need to implement our own zfill for uPython)'''
+    if len(str)>=digits:
+        return str
+    else:
+        return ((digits - len(str)) * '0')+str
+
+def ditlen(wpm):
+    '''calculates the length in ms of a morse code element for given words per minute'''
+    return 60000 / (50*wpm) #60000s / (50 elements (as in PARIS) * WPM)
+
+
+def encode_morse(text,wpm):
+    '''creates an bytes for ending throught a socket'''
+
+    """Protocol description:
+    This may be compatible with the morserino protocol
+
+    
+    protocolversion: 2 bits
+    serial number: 6 bits
+    morse speed: 6 bits
+    text: variable length of 2bit characters
+
+    01 = dit
+    10 = dah
+    00 = End of Character
+    11 = End of Word
+    """
+
+    global serial
+
+    #create 14 bit header
+    m = zfill(bin(protocol_version)[2:],2) #2bits for protocol_version
+    m += zfill(bin(serial)[2:],6) #6bits for serial number
+    m += zfill(bin(wpm)[2:],6) #6bits for words per minute
+
+    #add payload
+    for c in text:
+        if c == ' ':
+            continue #skip space characters
+
+        for e in morse[c.lower()]:
+            if e == '.':
+                m += '01' #dit
+            else:
+                m += '10' #dah
+
+        m += '00' #End of character
+    m = m[0:-2] + '11' #End of word
+
+    m = m.ljust(int(8*ceil(len(m)/8.0)),'0') #fill in incomplete byte
+
+    res = ''
+    for i in range(0, len(m),8):
+        res += chr(int(m[i:i+8],2))
+
+    serial +=1
+    return res.encode()
+
+
+def decode_header(unicodestring):
+    '''converts a received morse code byte string and returns a list
+    with the header info [protocol_v, serial, wpm]''' 
+    bytestring = unicodestring.decode()
+    bitstring = ''
+
+    for byte in bytestring:
+        integer = ord(byte)
+        bitstring += f'{integer:08b}'
+    
+    m_protocol = int(bitstring[:2],2)
+    m_serial = int(bitstring[3:8],2)
+    m_wpm = int(bitstring[9:14],2)
+    return [m_protocol, m_serial, m_wpm]
+
+
+def decode_payload(unicodestring):
+    '''converts a received morse code byte sting to text''' 
+    bytestring = unicodestring.decode()
+    bitstring = ''
+    for byte in bytestring:
+        integer = ord(byte)
+        bitstring += f'{integer:08b}'
+        
+    m_payload = bitstring[14:] #we just need the payload here
+
+    text = ''
+    #parse morse code elements
+    charbuffer ='' 
+    for i in range(0, len(m_payload),2):
+        sym = m_payload[i]+m_payload[i+1]
+
+        if sym == '01': #a dit
+            charbuffer += '.'
+            
+        elif sym == '10': #a dah
+            charbuffer += '-'
+            
+        elif sym == '00': #eoc
+            rec=0
+            for key,value in morse.items():
+                if value == charbuffer:
+                    text = text + key
+                    charbuffer=''
+                    rec = 1
+                    break
+            if not rec: #unknown character
+                text += '*'
+                
+        elif sym == '11': #eow
+            rec=0
+            for key,value in morse.items():
+                if value == charbuffer:
+                    text = text + key
+                    charbuffer=''
+                    rec=1
+                    break
+            if not rec: #unknown character
+                text += '*'
+
+            return text
+
+        
+
+class ipcwSocket():
+
+    def __init__(self,url=('0.0.0.0',7373),timeout=10):
+        self.serversock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        self.serversock.bind(url)
+        self.serversock.settimeout(timeout)
+        self.serversock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, True)
+        debug("Server created")
+
+
+    def sendto(self, data, url):
+        self.serversock.sendto(data,url)
+
+    def recv(self):
+        data=None
+        try:
+            data = self.serversock.recv(64)
+        except socket.timeout:
+            print("timeout")
+        if data:
+            return[decode_header(data),decode_payload(data)]
+
+if __name__ == "__main__":
+    print(decode_payload(encode_morse('DJ5?E/p',20)))
+    print(decode_header(encode_morse('DJ5?E/p',20)))
+    s = ipcwSocket()
+    s.sendto(encode_morse('servertest',20),('255.255.255.255',7373))
+    time.sleep(0)
+    print(s.recv())
+
---
+++ ipcwTRX.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+#Author: Sebastian Stetter  (DJ5SE)
+#License GPL v3
+
+#This file implements ipcwTRX trasceiver for NodeMCU / MicroPython
+
+import machine, network, utime as time
+from ipcw import *
+
+### configuration ###
+tx_url = '255.255.255.255'
+port = 7373
+s_ssid = ''
+s_key = ''
+ap_ssid = 'ipcw_ap'
+ap_key = 'ipcw'
+ap_wifi_ch = 11
+wpm = 20
+tx_sidetone = 550.0
+rx_sidetone = 600.0
+txrx_delay = 500 #ms
+
+###########
+# state variables #
+ip_address = None #own ip address, will be set by setup_server function
+state_tx = False #are we sending?
+state_menu = False #are we in the menu?
+dit_down = False
+dah_down = False
+last_lever_down = None
+last_key_time=None
+
+###########
+
+def setup_network(ssid='ipcw_ap', key='ipcw'):
+    try: #only if we are in uPython
+        import network
+        #check if we have a ssid different than 'ipcw_ap'
+        if not ssid == 'ipcw_ap':
+            #connect to given ssid
+            pass
+            return True
+        elif False: #try if we find network named 'ipcw_ap'
+            try:
+                #to connect to it using standard credentials
+            except:
+                continue
+            return True
+        else:
+            #we seem to be first, lets create an access point named 'ipcw_ap' for other devices
+            return True
+
+        
+def setup_gpios():
+    try:
+        import machine
+        #setup the pins and pwm for sidetone here
+        
+        return True
+    except:
+        #not on uPython
+        return False
+
+def setup_server():
+    #see if we have an ip address
+    if ip_address:
+        socket = ipcwSocket((ip_address,port))
+        return True
+    else:
+        return False
+
+
+def sidetone(freq, duration)
+    #code for PWM side tone here
+    pass
+
+def process_key():
+    '''checks the key io pins and handles keying,sidetone, as well as character recognition'''
+    pass
+
+def play_string_as_morse(string, freq, wpm):
+    '''plays the given string as morse code'''
+    ell=ditlen(wpm)
+    for c in string:
+        if c ==' ':
+            utime.sleep_ms(ell*6) #wordspacing
+        else:
+            for e in morse[c.lower()]:
+                if e == '.':
+                    sidetone(freq,ell)
+                    time.sleep_ms(ell)
+                else:
+                    sidetone(freq,ell*3)
+                    time.sleep_ms(ell)
+    utime.sleep(ell*2) #charcter spacing only 2x becaus ewe have a charspace already
+
+
+def play_recvd(data):
+    '''play morse received via socket'''
+    p,s,wpm= decode_header(data)
+    ell = ditlen(wpm)
+     
+    bytestring = data.decode()
+    bitstring = ''
+    for byte in bytestring:
+        integer = ord(byte)
+        bitstring += f'{integer:08b}'
+        
+    m_payload = bitstring[14:] #we just need the payload here
+
+    #play morse code elements
+    for i in range(0, len(m_payload),2):
+        sym = m_payload[i]+m_payload[i+1]
+
+        if sym == '01': #a dit
+            sidetone(rx_sidetone,ell)
+            time.sleep(ell)
+        elif sym == '10': #a dah
+            sidetone(rx_sidetone,ell*3)
+            time.sleep(ell)
+        elif sym == '00': #eoc
+            time.sleep(ell*4)    
+        elif sym == '11': #eow
+            time.sleep(ell*6)
+    
